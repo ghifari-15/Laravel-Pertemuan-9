371718811f275767bb73d87bbdac4e7e2e8343cd
Grappari <command@gmail.com>
2024-11-11 14:12:37 +0700

    first commit

---
+++ routes/BiodataController.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BiodataController extends Controllers
+{
+    public function home() {
+        return "Selamat datang Reksi Cina";
+    }
+    public function about(){
+        return "awokawokawok";
+    }
+}
+
+
+?>
